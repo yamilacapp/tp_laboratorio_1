679b8a7f1caca67da8add6ea878af70b81092cd7
yamilacapp <yamila_capp@hotmail.com>
2016-09-06 13:46:51 -0300

    Segunda Parte del Tp1
    Segunda Parte del Tp1

---
+++ tp1 2p/menu.h
@@ -0,0 +1,8 @@
+
+int verificar(int opcion);
+int menu(float num1, float num2, int flag1, int flag2);
+float sumar(float x, float y);
+float restar(float x, float y);
+float multiplicacion(float x, float y);
+float division(float x, float y);
+float factorial(int x);
